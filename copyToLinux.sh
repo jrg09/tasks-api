ccbc3b0b4e1afca6613f8c8fb3c1b2fa6d7fed52
Jorge Mac <jorge.mac@jrg9.com>
2025-01-08 13:36:10 -0600

    tasks & gastos ok

---
+++ copyToLinux.sh
@@ -0,0 +1,1 @@
+scp -r controllers db index.js middleware models package-lock.json package.json public routes validators vercel.json jrg@192.168.192.110:/home/jrg/code/node/tasks
---
+++ testCurl.sh
@@ -0,0 +1,1 @@
+curl -H 'user-id:1' http://192.168.192.110:8099/api/v1/payments
